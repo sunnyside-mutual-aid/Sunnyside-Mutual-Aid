e7b76551b5273ebb0c5e2e5bf947224dd4473fc5
Mark Beasley <mbeasl1@gmail.com>
2020-04-22 23:00:05 -0400

    Add file upload to markdown editor

--- project/project_folder/urls.py
+++ project/project_folder/urls.py
@@ -4,10 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 
+from website.views import admin_uploader
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('martor/', include('martor.urls')),
+    path('api/uploader/', admin_uploader, name='markdown_uploader_page'),
 ]
 
 urlpatterns += i18n_patterns(
--- project/website/views.py
+++ project/website/views.py
@@ -1,5 +1,15 @@
+import os
+import json
+import uuid
+
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
+
+from martor.utils import LazyEncoder
 
 from .models import Page
 
@@ -18,10 +28,52 @@ def volunteer(request):
 
 
 def get_page(request, slug):
-    page = get_object_or_404(
-        Page.objects.filter(published=True),
-        slug=slug
-    )
+    page = get_object_or_404(Page.objects.filter(published=True), slug=slug)
     return render(request, 'page.html', {
         'page': page
     })
+
+
+@login_required
+def admin_uploader(request):
+    """
+    Makdown image upload for locale storage
+    and represent as json to markdown editor.
+    """
+    if request.method == 'POST' and request.is_ajax():
+        if 'markdown-image-upload' in request.FILES:
+            image = request.FILES['markdown-image-upload']
+            image_types = [
+                'image/png', 'image/jpg',
+                'image/jpeg', 'image/pjpeg', 'image/gif'
+            ]
+            if image.content_type not in image_types:
+                data = json.dumps({
+                    'status': 405,
+                    'error': 'Bad image format.'
+                }, cls=LazyEncoder)
+                return HttpResponse(
+                    data, content_type='application/json', status=405)
+
+            if image.size > settings.MAX_IMAGE_UPLOAD_SIZE:
+                to_MB = settings.MAX_IMAGE_UPLOAD_SIZE / (1024 * 1024)
+                data = json.dumps({
+                    'status': 405,
+                    'error': 'Maximum image file is %(size) MB.' % {'size': to_MB}
+                }, cls=LazyEncoder)
+                return HttpResponse(
+                    data, content_type='application/json', status=405)
+
+            img_uuid = "{0}-{1}".format(uuid.uuid4().hex[:10], image.name.replace(' ', '-'))
+            tmp_file = os.path.join(settings.MARTOR_UPLOAD_PATH, img_uuid)
+            def_path = default_storage.save(tmp_file, ContentFile(image.read()))
+            img_url = os.path.join(settings.MEDIA_URL, def_path)
+
+            data = json.dumps({
+                'status': 200,
+                'link': img_url,
+                'name': image.name
+            })
+            return HttpResponse(data, content_type='application/json')
+        return HttpResponse('Invalid request!')
+    return HttpResponse('Invalid request!')
